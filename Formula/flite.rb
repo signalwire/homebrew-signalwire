f3121907712137c64303c8f5d22b81ec717bacf9
Mike Jerris <mike@jerris.com>
2016-07-08 13:48:54 -0400

    FS-9335, github issue #3, fix sha256 sum for flite tarball

--- Formula/flite.rb
+++ Formula/flite.rb
@@ -2,7 +2,7 @@ class Flite < Formula
   desc "flite"
   homepage "https:/freeswitch.org/stash/projects/SD/repos/libflite"
   url "http://files.freeswitch.org/downloads/libs/flite-2.0.0.tar.gz"
-  sha256 "fe3b4b653739d714d47042333d3634ea0ab93362addd4e968840f837045dd345"
+  sha256 "3a66c544373c5905fafb1314bc0e4988ef9e0993b79d1448f68a756ca8b96e21"
 
   def install
     args = ["--disable-dependency-tracking", "--prefix=#{prefix}"]
