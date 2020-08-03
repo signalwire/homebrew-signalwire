c32734f40bb884d8d1a45139ac573b4eb6c64a8a
Mike Jerris <mike@signalwire.com>
2020-08-03 17:41:16 -0400

    fix hash

--- Formula/sofia-sip.rb
+++ Formula/sofia-sip.rb
@@ -2,7 +2,7 @@ class SofiaSip < Formula
   desc "sofia-sip"
   homepage "https://github.com/freeswitch/sofia-sip"
   url "https://github.com/freeswitch/sofia-sip/archive/f5d44540d32b646133f685f04cb8b6658f748714.zip"
-  sha256 "49b9db6e3a8c4882eb4a2e0ca090c5c4f0223ab7cb5fb60ee7c198dafc90f230"
+  sha256 "7845967bd7891849ed239173568c90d376d11b9d53d6e7f4c4565f06150fc71a"
 
   def install
     system "./bootstrap.sh"
