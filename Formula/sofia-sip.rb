a01e58ae756517527f33dd861f15cc2c04734212
Mike Jerris <mike@signalwire.com>
2020-08-03 17:55:24 -0400

    update

--- Formula/sofia-sip.rb
+++ Formula/sofia-sip.rb
@@ -1,11 +1,10 @@
 class SofiaSip < Formula
   desc "sofia-sip"
   homepage "https://github.com/freeswitch/sofia-sip"
-  url "https://github.com/freeswitch/sofia-sip/archive/f5d44540d32b646133f685f04cb8b6658f748714.zip"
+  url "https://freeswitch.org/bamboo/browse/SP-SPTD-3/artifact/shared/sofia-sip-pre-bootstrapped-tarball/sofia-sip-1.12.12-f5d44540d3.tar.gz"
   sha256 "7845967bd7891849ed239173568c90d376d11b9d53d6e7f4c4565f06150fc71a"
 
   def install
-    system "./bootstrap.sh"
     args = ["--disable-dependency-tracking", "--prefix=#{prefix}", "--with-pic", "--with-glib=no", "--disable-stun"]
     system "./configure", *args
     system "make"
