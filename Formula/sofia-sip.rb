464a3b901f9b50de9a64ac3491f87643fbe0676f
Mike Jerris <mike@signalwire.com>
2020-08-03 17:59:09 -0400

    update

--- Formula/sofia-sip.rb
+++ Formula/sofia-sip.rb
@@ -4,6 +4,8 @@ class SofiaSip < Formula
   url "https://freeswitch.org/bamboo/browse/SP-SPTD-3/artifact/shared/sofia-sip-pre-bootstrapped-tarball/sofia-sip-1.12.12-f5d44540d3.tar.gz"
   sha256 "2c7d3e5217df807e2d9a7f3f17da3da9e7b134e56201541ff3d95619a3c6f502"
 
+  depends_on "openssl"
+
   def install
     args = ["--disable-dependency-tracking", "--prefix=#{prefix}", "--with-pic", "--with-glib=no", "--disable-stun"]
     system "./configure", *args
