1d12a40357b836b6af1f1232ebe37003d5cc5f55
Mike Jerris <mike@signalwire.com>
2020-08-04 18:56:59 -0400

    up

--- Formula/spandsp.rb
+++ Formula/spandsp.rb
@@ -2,7 +2,7 @@ class Spandsp < Formula
   desc "spandsp"
   homepage "https://github.com/freeswitch/spandsp"
   url "https://files.freeswitch.org/downloads/libs/spandsp-0.0.6-7a0c2d5641.tar.gz"
-  sha256 "2c7d3e5217df807e2d9a7f3f17da3da9e7b134e56201541ff3d95619a3c6f502"
+  sha256 "73ef6e85194df2693299b74836f5c1ff1298c480d5705c4d6d4e6ffb8caf8c99"
 
   depends_on "libtiff"
   depends_on "libjpeg"
