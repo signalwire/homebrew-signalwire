edb5f3d68c7850ae651f41cb8f4635dea980d6d7
Mike Jerris <mike@signalwire.com>
2021-08-30 10:29:32 -0600

    v1.13.5

--- Formula/sofia-sip.rb
+++ Formula/sofia-sip.rb
@@ -1,7 +1,7 @@
 class SofiaSip < Formula
   desc "sofia-sip"
   homepage "https://github.com/freeswitch/sofia-sip"
-  url "https://files.freeswitch.org/downloads/libs/sofia-sip-1.13.4-6901dbf09b.tar.gz"
+  url "https://files.freeswitch.org/downloads/libs/sofia-sip/sofia-sip-1.13.5-afa7ebb814.tar.gz"
   sha256 "10c2b31b61db6f5719a9bae7d356f6f513760c2c5c12d0c5c2720d46ddf415b6"
 
   depends_on "openssl"
