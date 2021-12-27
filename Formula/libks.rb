10cf6ad4169ff7e6fda992e1d603565665522bd9
Andrey Volk <andywolk@gmail.com>
2021-12-27 19:10:53 +0300

    Update libks to 1.7.1

--- Formula/libks.rb
+++ Formula/libks.rb
@@ -1,8 +1,8 @@
 class Libks < Formula
   desc "libks"
   homepage "https://github.com/signalwire/libks"
-  url "http://files.freeswitch.org/downloads/libs/libks-1.1.0.tar.gz"
-  sha256 "df685ad4374b2e6196fb5c28013a9fd1effe4a3a18b999f7633711713546b858"
+  url "http://files.freeswitch.org/downloads/libs/libks-1.7.1.tar.gz"
+  sha256 "00520fa94121be1addcaa860d31fd0ea1c082339740fa518f0b61a6ac3794880"
 
 
   depends_on "cmake" => :build
