3494780632d3a4984592f22176dc6cb1d475ff54
Andrey Volk <andywolk@gmail.com>
2022-05-19 12:43:40 +0300

    Update signalwire-c to 1.3.2

--- Formula/signalwire-c.rb
+++ Formula/signalwire-c.rb
@@ -1,8 +1,8 @@
 class SignalwireC < Formula
   desc "signalwire-c"
   homepage "https://github.com/signalwire/signalwire-c"
-  url "https://files.freeswitch.org/downloads/libs/signalwire-client-c-1.3.1.tar.gz"
-  sha256 "4ada7699a5565f339258fde8156a34155733264c12b1a3709d9b8aeabfb75815"
+  url "https://files.freeswitch.org/downloads/libs/signalwire-client-c/signalwire-client-c-1.3.2.tar.gz"
+  sha256 "d6b68288ab36cf3860798263900a9bb0481d87ac9fc18d1c7d932682c8a1db06"
 
 
   depends_on "cmake" => :build
