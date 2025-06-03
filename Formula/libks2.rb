c34e1aacedf506073d90613c48f48c140d99b700
Andrey Volk <andywolk@gmail.com>
2025-06-03 23:52:58 +0300

    ks_pid_t

--- Formula/libks2.rb
+++ Formula/libks2.rb
@@ -1,8 +1,8 @@
 class Libks2 < Formula
   desc "libks2"
   homepage "https://github.com/signalwire/libks"
-  url "https://github.com/signalwire/libks/archive/v2.0.2.tar.gz"
-  sha256 "af94f9fcdb2022b8f09187309ac2d372a5a4cc639af77cd4375f2d5c88b4fd63"
+  url "https://github.com/signalwire/libks/archive/349ed475e1b1c5ba94342d99819b9062e5e008cc.tar.gz"
+  sha256 "19a6bb6b17897466f162d0000da7885afcd68ce3f231518a630cd9823c33bdba"
 
 
   depends_on "cmake" => :build
