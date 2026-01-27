f43d3715ffc6a1e5dd7f042cf4eb25a5a3d083c2
Andrey Volk <andywolk@gmail.com>
2026-01-27 19:02:13 +0300

    Update libks2 to 2.0.9

--- Formula/libks2.rb
+++ Formula/libks2.rb
@@ -1,8 +1,8 @@
 class Libks2 < Formula
   desc "libks2"
   homepage "https://github.com/signalwire/libks"
-  url "https://github.com/signalwire/libks/archive/v2.0.7.tar.gz"
-  sha256 "9603cad9a7538070050743b6a686d44927140221c07b859c6e1196842ce0080a"
+  url "https://github.com/signalwire/libks/archive/v2.0.9.tar.gz"
+  sha256 "c3a352b49a0452a710e80ad9c19a318c4d946a33b5ff6094617f3d4cb9ca02e2"
 
 
   depends_on "cmake" => :build
