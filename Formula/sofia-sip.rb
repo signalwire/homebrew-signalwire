da04dfcb0c91e4dd2a6bfc72a5d73e389663deeb
Andrey Volk <andywolk@gmail.com>
2026-08-04 01:05:07 +0300

    Update sofia-sip to v1.13.18

--- Formula/sofia-sip.rb
+++ Formula/sofia-sip.rb
@@ -1,12 +1,18 @@
 class SofiaSip < Formula
   desc "sofia-sip"
   homepage "https://github.com/freeswitch/sofia-sip"
-  url "https://files.freeswitch.org/downloads/libs/sofia-sip/sofia-sip-1.13.10-2cb3820d9f.tar.gz"
-  sha256 "962889154a8a8527650d26999227c00f717311a3e31a2c4f34bc1b041d133c22"
+  url "https://github.com/freeswitch/sofia-sip/archive/refs/tags/v1.13.18.tar.gz"
+  sha256 "d2ad4e64753a7c9843b766b8de8081d9c1d7acfaeb53c12b3aed7fdb9235766c"
 
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
   depends_on "openssl"
 
   def install
+    # The GitHub tarball ships no configure script, only configure.ac.
+    system "./autogen.sh"
+
     args = ["--disable-dependency-tracking", "--prefix=#{prefix}", "--with-pic", "--with-glib=no", "--disable-stun"]
     system "./configure", *args
     system "make"
